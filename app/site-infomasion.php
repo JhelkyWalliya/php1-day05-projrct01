d8e8bb1e92d5b0e35f79dfb36f8ea4ad93c602d7
JhelkyWalliya <jhelky18411061@yahoo.com>
2022-06-06 04:17:00 -0700

    final project 05

---
+++ app/site-infomasion.php
@@ -0,0 +1,16 @@
+<?php 
+
+$site_title = " highland roastery";
+$site_description = "roasting house and coffee shop in papua";
+$tag_ilne = " good farme - best coffee - better Qualiy";
+
+$site_logo = "assts/img/logo-ht.png";
+
+$copyringht_info ="copyright &copy; 2022";
+$author = "Jhelky Walliya";
+
+
+$instragaram = "https://www.instagram.com/highlandroastery/?__coig_restricted=1";
+$facebook = +"https//facebbok.com";
+$youtube ="https//youtube.com";
+$linkedin = "";
---
+++ view/404.php
@@ -0,0 +1,15 @@
+<section class="page-section cta">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-9 mx-auto">
+                        <div class="cta-inner bg-faded text-center rounded">
+                            <h2 class="section-heading mb-4">
+                                <span class="section-heading-upper">eror</span>
+                                <span class="section-heading-lower">404</span>
+                            </h2>
+                            <p class="mb-0">maaf halaman tidak di temukan</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
